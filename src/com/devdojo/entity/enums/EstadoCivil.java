aa037b73f31402ddd9fd3774cd7353da03ed6e99
DionatanLeao <dionatan.ti@outlook.com>
2020-11-23 21:33:23 -0300

    Tipos Primitivos, Exercício e Operadores

---
+++ src/com/devdojo/entity/enums/EstadoCivil.java
@@ -0,0 +1,10 @@
+package com.devdojo.entity.enums;
+
+public enum EstadoCivil {
+	solteiro, 
+	casado, 
+	separado_judicial,
+	separado_extrajudicialmente,
+	divorciado,
+	viúvo;
+}
---
+++ src/com/devdojo/entity/enums/Sexo.java
@@ -0,0 +1,7 @@
+package com.devdojo.entity.enums;
+
+public enum Sexo {
+	masculino,
+	feminino;
+
+}
