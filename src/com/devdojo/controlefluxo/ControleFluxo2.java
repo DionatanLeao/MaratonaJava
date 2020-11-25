0e86876131d2a191332c7f8195f1e2c192a0e0b1
DionatanLeao <dionatan.ti@outlook.com>
2020-11-25 10:00:35 -0300

    Tabela Verdade

---
+++ src/com/devdojo/controlefluxo/ControleFluxo2.java
@@ -0,0 +1,20 @@
+package com.devdojo.controlefluxo;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class ControleFluxo2 {
+
+	public static void main(String[] args) {
+		// ? if : else
+		int idade = 20;
+		String status;
+		
+		status = idade >= 18 ? "Adulto" : "Não Adulto";
+		
+		System.out.println(status);
+	}
+}
