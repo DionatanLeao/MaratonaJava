8c43b7700885c414d0bf7a08ce4ba54c0c2a7c88
DionatanLeao <dionatan.ti@outlook.com>
2020-11-25 10:26:22 -0300

    Controle de fluxo Resolução do exercício if else

--- src/com/devdojo/controlefluxo/ControleFluxo2.java
+++ src/com/devdojo/controlefluxo/ControleFluxo2.java
@@ -16,5 +16,25 @@ public class ControleFluxo2 {
 		status = idade >= 18 ? "Adulto" : "Não Adulto";
 		
 		System.out.println(status);
+		
+		System.out.println("------------------- Exercício ---------------------");
+		
+		double salario = 3600;
+		double totalImposto = 0;
+		
+		if(salario < 1000) {
+			totalImposto = salario * 0.05;
+		}
+		else if(salario >=1000 && salario <2000) {
+			totalImposto = salario * 0.1;
+		}
+		else if(salario >= 2000 && salario < 4000) {
+			totalImposto = salario * 0.15;
+		}
+		else {
+			totalImposto = salario * 0.2;
+		}
+		
+		System.out.println(totalImposto);
 	}
 }
