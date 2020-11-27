e00485f1be665fe5ebab03b10885068a6380583a
DionatanLeao <dionatan.ti@outlook.com>
2020-11-27 20:24:10 -0300

    Arrays parte 1

---
+++ src/com/devdojo/arrays/Arrays.java
@@ -0,0 +1,22 @@
+package com.devdojo.arrays;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Arrays {
+	public static void main(String[] args) {
+		int[] idades = new int[3];
+
+		idades[0] = 30;
+		idades[1] = 31;
+		idades[2] = 32;
+
+		System.out.println("idade 1: " + idades[0]);
+		System.out.println("idade 2: " + idades[1]);
+		System.out.println("idade 3: " + idades[2]);
+	}
+
+}
