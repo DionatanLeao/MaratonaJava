6f24570a86becb6a9646e18cf1f4ab1cbdf20d06
DionatanLeao <dionatan.ti@outlook.com>
2020-11-28 12:17:02 -0300

    Arrays parte 4 arrays multidimensionais

--- src/com/devdojo/arrays/Foreach.java
+++ src/com/devdojo/arrays/Foreach.java
@@ -8,9 +8,9 @@ package com.devdojo.arrays;
 
 public class Foreach {
 	public static void main(String[] args) {
-		int[] numeros = new int[5];
+//		int[] numeros = new int[5];
 		int[] numeros2 = { 1, 2, 3, 4, 5 };
-		int[] numeros3 = new int[] { 1, 2, 3, 4, 5 };
+//		int[] numeros3 = new int[] { 1, 2, 3, 4, 5 };
 
 		for (int i = 0; i < numeros2.length; i++) {
 			System.out.println(numeros2[i]);
