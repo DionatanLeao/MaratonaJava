34848110583d6ee0fa75dc3eb9255b58cfc8aa38
DionatanLeao <dionatan.ti@outlook.com>
2020-11-27 21:00:04 -0300

    Arrays parte 2

--- src/com/devdojo/arrays/Arrays.java
+++ src/com/devdojo/arrays/Arrays.java
@@ -17,6 +17,17 @@ public class Arrays {
 		System.out.println("idade 1: " + idades[0]);
 		System.out.println("idade 2: " + idades[1]);
 		System.out.println("idade 3: " + idades[2]);
+		
+		System.out.println("-------------------------------------");
+
+		String[] nomes = new String[3];
+		nomes[0] = "Maria";
+		nomes[1] = "João";
+		nomes[2] = "Manoel";
+
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.println((i+1) + " " + nomes[i]);
+		}
 	}
 
 }
