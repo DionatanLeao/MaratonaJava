1adfddb666ea10522e04e87fbc1da84cec2a4b99
DionatanLeao <dionatan.ti@outlook.com>
2020-12-01 21:17:22 -0300

    Solução do exercício introdução a classes

--- src/br/com/devdojo/javacore/test/ProfessorTest.java
+++ src/br/com/devdojo/javacore/test/ProfessorTest.java
@@ -10,6 +10,7 @@ import br.com.devdojo.javacore.classes.Professor;
 
 public class ProfessorTest {
 	public static void main(String[] args) {
+
 		Professor professor = new Professor();
 		professor.setNome("Maria de Paiva");
 		professor.setMatricula(123456);
@@ -20,6 +21,19 @@ public class ProfessorTest {
 		System.out.println(professor.getMatricula());
 		System.out.println(professor.getRg());
 		System.out.println(professor.getCpf());
+		
+		System.out.println("\n------------------------------------\n");
+
+		Professor professor2 = new Professor();
+		professor2.setNome("Joana de Paula");
+		professor2.setMatricula(123456);
+		professor2.setRg("0987-654");
+		professor2.setCpf("987654321-00");
+
+		System.out.println(professor2.getNome());
+		System.out.println(professor2.getMatricula());
+		System.out.println(professor2.getRg());
+		System.out.println(professor2.getCpf());
 
 	}
 }
